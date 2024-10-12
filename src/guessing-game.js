e95e4654bdb62f34444938ab40e201668dd3737c
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2024-10-12 18:04:31 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+    }
 
     setRange(min, max) {
-
+        for (let i = 0; i <= max; i++) {
+            this.arr[i] = i;
+        }
     }
 
     guess() {
+        let right = this.arr.length - 1;
+        let middle = Math.ceil(right / 2);
 
+        return this.arr[middle];
     }
 
     lower() {
+        let guessNumber = this.guess();
+        let indexOfGuessNum = this.arr.indexOf(guessNumber);
 
+        this.arr = this.arr.slice(0, (indexOfGuessNum + 1));
     }
 
     greater() {
+        let guessNumber = this.guess();
+        let indexOfGuessNum = this.arr.indexOf(guessNumber);
 
+        this.arr = this.arr.slice(indexOfGuessNum);
     }
 }
 
